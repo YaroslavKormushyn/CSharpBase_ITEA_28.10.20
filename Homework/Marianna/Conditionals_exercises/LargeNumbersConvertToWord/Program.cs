4b10d04b0fd4f812d8cecd616dd398a050225de0
Marianna Prytula <mariannapritula@gmail.com>
2020-11-09 16:59:40 +0200

    Add conditionals and loops to Homework

---
+++ Homework/Marianna/Conditionals_exercises/LargeNumbersConvertToWord/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LargeNumbersConvertToWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] units = new string[] { "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine ", "ten ", "eleven ", "twelve ", "thirteen ", "fourteen ", "fifteen ", "sixteen ", "seventeen ", "eighteen ", "nineteen " };
+            string[] tens = new string[] { "ten ", "twenty ", "thirty ", "fourty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety " };
+            int i, num;
+            string result = "";
+            Console.Write("Enter the number in range 1-999 : ");
+            num = int.Parse(Console.ReadLine());
+            if (num > 99 && num < 1000)
+            {
+                i = num / 100;
+                result = units[i - 1] + "hundreed ";
+                num = num % 100;
+            }
+            if (num > 19 && num < 100)
+            {
+                i = num / 10;
+                result = result + tens[i - 1];
+                num = num % 10;
+            }
+            if (num < 20 && num > 0)
+                result += units[num - 1];
+            Console.WriteLine("The number is " + result);
+            Console.ReadLine();
+        }
+    }
+    }
+
---
+++ Homework/Marianna/Loops/RandomOrderNIntegers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RandomOrderNIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Random rand = new Random();
+                int temp, randomNumber;
+                Console.Write("Enter number: ");
+                int n = Int32.Parse(Console.ReadLine());
+                int[] arr = new int[n];
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = i;
+                }
+
+                foreach (int i in arr)
+                {
+                    randomNumber = rand.Next(0,n);
+                    temp = arr[i];
+                    arr[i] = arr[randomNumber];
+                    arr[randomNumber] = temp;
+                }
+                Console.WriteLine("Array of random integers");
+                foreach (int i in arr) Console.Write(arr[i]+ " ");
+            }
+        }
+    }
+}
+    
+
