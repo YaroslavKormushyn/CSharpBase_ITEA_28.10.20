9ba37e396b4643c757d12ddd3a4c4067256c63d2
Anastasiya Nerodenko <anastasiyanerodenko@gmail.com>
2020-11-12 17:21:17 +0200

    Homework 1

---
+++ Homework/Anastasiya/Loops Exercises/Fibonacci sequence/Fibonacci sequence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fibonacci_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb1 = 0, numb2 = 1;
+
+            Console.Write("Enter n to print the sum of the first n members: ");
+            int n = int.Parse (Console.ReadLine());
+
+            Console.Write("0,");
+
+            for (int i = 1; i < n; i++)
+            {
+                int numb3 = numb1 + numb2;
+                numb1 = numb2;
+                numb2 = numb3;
+
+                Console.Write ("{0},", numb3);
+            }
+        }
+    }
+}
---
+++ Homework/Anastasiya/Loops Exercises/Not divisible by 3 and 7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Not_divisible_by_3_and_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                if (i % (3 * 7) != 0) Console.WriteLine(i);
+            }
+        }
+    }
+}
