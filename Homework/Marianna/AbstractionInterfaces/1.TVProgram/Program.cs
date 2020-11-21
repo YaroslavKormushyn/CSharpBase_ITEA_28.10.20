76539be0031342dc2fb6287d24851b4abe5a7282
Marianna Prytula <mariannapritula@gmail.com>
2020-11-21 22:14:57 +0200

    Use LINQ extension methods in Task1_3

--- Homework/Marianna/AbstractionInterfaces/1.TVProgram/Program.cs
+++ Homework/Marianna/AbstractionInterfaces/1.TVProgram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 namespace _1.TVProgram
 {
@@ -18,26 +19,52 @@ namespace _1.TVProgram
             programs[8] = new Cartoon("Gravity Falls", 2, DateTime.Parse("2020, 09, 20, 13:30:01 PM"), DateTime.Parse("2020, 09, 20, 15:30:01 PM"));
             programs[9] = new News("The Age of Mankind documentary", 1, DateTime.Parse("2020, 12, 04, 14:30:01 PM"), DateTime.Parse("2020, 12, 04, 18:30:01 PM"));
             Console.WriteLine("Descending order by Rating column:");
-            var qry = from p in programs
-                      orderby p.Rating descending
-                      select p;
-            Array.ForEach<TvProgram>(qry.ToArray<TvProgram>(), p => Console.WriteLine("The name of TvProgram: {0}. Rating:  {1}  ", p.Name, p.Rating));
+            IEnumerable<TvProgram> query = programs.OrderByDescending(program => program.Rating);
+            foreach (TvProgram program in query)
+            {
+                Console.WriteLine("The name of TvProgram: {0}. Rating:  {1}  ", program.Name, program.Rating);
+            }
             Console.WriteLine();
 
             Console.WriteLine("Output science programs which air after (2020, 11, 20) point in time:");
             var greaterThanThis = new DateTime(2020, 11, 20);
-            var qry1 = from k in programs
-                       where k.Start > greaterThanThis && k is Science
-                       select k;
-            Array.ForEach<TvProgram>(qry1.ToArray<TvProgram>(), k => Console.WriteLine("The name of TvProgram: {0} the air time: from {1} to  {2} ", k.Name, k.Start, k.End));
+            IEnumerable<TvProgram> query1 = programs.Where((program) => program.Start > greaterThanThis && program is Science);
+
+            foreach (TvProgram program in query1)
+            {
+                Console.WriteLine("The name of TvProgram: {0} the air time: from {1} to  {2} ", program.Name, program.Start, program.End);
+            }
             Console.WriteLine();
 
             Console.WriteLine("Output programs which are longer than two hours:");
             TimeSpan aTwo = new System.TimeSpan(2, 0, 0);
-            var qry2 = from n in programs
-                       where n.End.Subtract(n.Start) > aTwo
-                       select n;
-            Array.ForEach<TvProgram>(qry2.ToArray<TvProgram>(), n => Console.WriteLine("The name of TvProgram: {0} the air time: from {1} to  {2}  TimeSpan:{3} ", n.Name, n.Start, n.End, n.End.Subtract(n.Start)));
+            IEnumerable<TvProgram> query2 = programs.Where((program) => program.End.Subtract(program.Start) > aTwo);
+            foreach (TvProgram program in query2)
+            {
+                Console.WriteLine("The name of TvProgram: {0} the air time: from {1} to  {2}  TimeSpan:{3} ", program.Name, program.Start, program.End, program.End.Subtract(program.Start));
+            }
+            /*
+           var qry = from p in programs
+                     orderby p.Rating descending
+                     select p;
+           Array.ForEach<TvProgram>(qry.ToArray<TvProgram>(), p => Console.WriteLine("The name of TvProgram: {0}. Rating:  {1}  ", p.Name, p.Rating));
+           Console.WriteLine();
+
+           Console.WriteLine("Output science programs which air after (2020, 11, 20) point in time:");
+           var greaterThanThis = new DateTime(2020, 11, 20);
+           var qry1 = from k in programs
+                      where k.Start > greaterThanThis && k is Science
+                      select k;
+           Array.ForEach<TvProgram>(qry1.ToArray<TvProgram>(), k => Console.WriteLine("The name of TvProgram: {0} the air time: from {1} to  {2} ", k.Name, k.Start, k.End));
+           Console.WriteLine();
+
+           Console.WriteLine("Output programs which are longer than two hours:");
+           TimeSpan aTwo = new System.TimeSpan(2, 0, 0);
+           var qry2 = from n in programs
+                      where n.End.Subtract(n.Start) > aTwo
+                      select n;
+           Array.ForEach<TvProgram>(qry2.ToArray<TvProgram>(), n => Console.WriteLine("The name of TvProgram: {0} the air time: from {1} to  {2}  TimeSpan:{3} ", n.Name, n.Start, n.End, n.End.Subtract(n.Start)));
+           */
         }
     }
 }
