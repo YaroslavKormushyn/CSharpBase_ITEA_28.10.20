5e046f93d44411879d4f2cd1a337691d713769d9
Marianna Prytula <mariannapritula@gmail.com>
2020-11-26 14:20:02 +0200

    Add part of program 1 to the Homework 3

---
+++ Homework/Marianna/ClassesObjectsInheritance/PersonStudentTeacher/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonStudentTeacher
+{
+    class Student : Person
+    {
+        public Student(string firstName, string lastName, int age) : base(firstName, lastName, age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        public Teacher Teacher { get; set; }
+
+        public void GoToClasses()
+        {
+            Console.WriteLine(" I’m going to class.{0}", FirstName);
+        }
+        public void ShowAge(int n)
+        {
+            Console.WriteLine("Age is: " + n);
+        }
+        public override void Perform()
+        {
+        }
+    }
+}
+
